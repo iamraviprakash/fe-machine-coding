3ce887cd868c8336b4a5ef0e0b94bbb90efea2c0
Ravi Prakash <raviprakash.coder@gmail.com>
2021-09-05 04:01:21 +0000

    feat: implement using requestAnimationFrame

--- script.js
+++ script.js
@@ -1,63 +1,71 @@
-const totalHorizontalCells = 10;
 
 function ProgressBarLoader(element, buttonElement, timeDuration) {
   // State
   let requestsInQueue = 0;
-  let progressCount = 0;
-  let fillIntervalIds = [];
+  let start, previousTimeStamp;
 
   // Initialisation
   const loader = document.getElementById(element);
   const runButton = document.getElementById(buttonElement);
 
-  runButton.addEventListener("click", (e) => {
+  const updateButton = () => {
+    runButton.innerHTML = `Run ${requestsInQueue > 1 ? requestsInQueue : ""}`;
+  }
 
+  const fillLoader = (timestamp) => {
+    if (start === undefined) {
+      start = timestamp;
+    }
+
+    const elapsed = timestamp - start;
+
+    if (previousTimeStamp !== timestamp) {
+      const count = Math.min((100 / timeDuration) * elapsed, 100);
+      loader.style.width = count + '%';
+    }
+
+    if (elapsed < timeDuration) {
+      previousTimeStamp = timestamp
+      window.requestAnimationFrame(fillLoader);
+    } else {
+      dequeue();
+      resetState();
+      updateButton();
+      resetLoader();
+
+      if (requestsInQueue > 0) {
+        startLoader();
+      }
+    }
+  }
+
+  const dequeue = () => {
+    requestsInQueue--;
+  }
+
+  const enqueue = () => {
+    requestsInQueue++;
+  }
+
+  const resetState = () => {
+    start = undefined;
+    previousTimeStamp = undefined;
+  }
+
+  const resetLoader = () => {
+    loader.style.width = '0%';
+  }
+
+  const startLoader = () => {
+    window.requestAnimationFrame(fillLoader)
+  }
+
+  runButton.addEventListener("click", (e) => {
     if (requestsInQueue == 0) {
       startLoader();
     }
 
-    requestsInQueue += 1;
+    enqueue();
     updateButton();
   });
-
-  const updateButton = () => {
-     runButton.innerHTML = `Run ${requestsInQueue > 1 ? requestsInQueue : ""}`;
-  }
-
-  const fillLoader = () => {
-    if (progressCount > (totalHorizontalCells - 1) ){
-      requestsInQueue--;
-      progressCount = 0;
-
-      updateButton();
-      resetLoader();
-
-      if (requestsInQueue != 0) {
-        startLoader();
-      }
-
-      return;
-    }
-
-    let child = document.createElement("DIV");
-    child.classList.add("filledCell");
-
-    progressCount++;
-    loader.appendChild(child);
-  }
-
-  const resetLoader = () => {
-    while (loader.firstChild) {
-      loader.removeChild(loader.firstChild);
-    }
-
-    window.clearTimeout(fillIntervalIds[0]);
-    fillIntervalIds.shift();
-  }
-
-  const startLoader = () => {
-    let intervalKey = setInterval(fillLoader, timeDuration / totalHorizontalCells);
-
-    fillIntervalIds.push(intervalKey);
-  }
 }
