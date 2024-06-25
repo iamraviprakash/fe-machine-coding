61a7d2eec3a71eb6d348f5a93c9ab857c26ae20a
Ravi Prakash <ravi@toddleapp.com>
2024-06-25 07:19:54 +0530

    Merge remote-tracking branch 'multi-column-sortable-tag-list/master'

---
+++ multi-column-sortable-tag-list/build/precache-manifest.f1c7f1b486299ba22ed88bd2f0649473.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "699ab88ade77171de490",
+    "url": "/static/js/main.699ab88a.chunk.js"
+  },
+  {
+    "revision": "13b849cb64ee7749944b",
+    "url": "/static/js/1.13b849cb.chunk.js"
+  },
+  {
+    "revision": "699ab88ade77171de490",
+    "url": "/static/css/main.80ce824b.chunk.css"
+  },
+  {
+    "revision": "752407972d5053627008abd873fdc951",
+    "url": "/index.html"
+  }
+];
